eff28842d166ffe247fae6f387f856a1ae126067
Yusuf Ozturk <yusuf.ozturk3@hva.nl>
2023-11-30 14:59:13 +0100

    log in gefixed

--- src/databaseQuery.ts
+++ src/databaseQuery.ts
@@ -1,13 +1,31 @@
 import "./config";
 import { api, session } from "@hboictcloud/api";
 
+
+export async function checkEmailExists(email: string): Promise<boolean> {
+    try {
+ 
+        const result: { count: number }[] | any = await api.queryDatabase("SELECT COUNT(*) AS count FROM user WHERE email = ?", [email]);
+ 
+        if (typeof result === "string") {
+            throw new Error(`Database error: ${result}`);
+        }
+
+        const count: number = result[0].count;
+        return count > 0;
+    } catch (error) {
+ 
+        console.error("Error checking if email exists:", error);
+        throw error;
+    }
+}
+
 export async function createAccount(email: string, username: string, firstname: string, lastname: string, password: string): Promise<number | undefined> {
     try {
         const createAccountString: string = "INSERT INTO user (email, username, firstname, lastname, password) VALUES ( ? , ? , ? , ? , ? )";
         const registerData: any = await api.queryDatabase(createAccountString, email, username, firstname, lastname, password);
         const userId: number = Number(registerData);
- 
- 
+
         return userId;
     } catch (error) {
         console.error("Error creating account:", error);
@@ -15,25 +33,59 @@ export async function createAccount(email: string, username: string, firstname: 
     }
 }
 
-export async function validateLogin(email: string ): Promise<undefined> {
+export async function validateLogin(email: string, password: string): Promise<number | undefined> {
     try {
         const validateLoginString: string = "SELECT email, password, userid FROM user WHERE email = ?";
  
         const userData: any = await api.queryDatabase(validateLoginString, email);
  
-        if (!userData[0]) {
-            alert("This user doesn't exist");
-            return;
-        }else {
-            session.set("email", userData[0].email);
-            session.set("password", userData[0].password);
-            session.set("userid", userData[0].userid);
-            window.location.replace("./index.html");
-            return;
+        if (userData.length === 0) {
+            console.log("no users found");
+            alert("no users found");
+            return undefined;
         }
  
+        const user: {
+            userid: number,
+            email: string,
+            password: string,
+        } = userData[0];
+
+        if (user.password === password) {
+            return user.userid;
+        } else {
+            return undefined;
+        }
     } catch (error) {
         console.error("Error during login:", error);
+        throw error;
+    }
+}
+
+
+export async function getInfoData (userid: number): Promise<any> {
+    try{
+        const getInfoDataString: string = "SELECT email, username, firstname, lastname, password FROM user WHERE userid = ?";
+        const getData: any = await api.queryDatabase(getInfoDataString, userid);
+
+        if(getData.length === 0) {
+            console.log("couldnt get user info.");
+        } else {
+            const user:  {
+                userid: number,
+                email: string, 
+                firstname: string,
+                lastname: string,
+                password: string,
+            } = getData[0];
+
+            sessionStorage.setItem("email", user.email),
+            sessionStorage.setItem("firstname", user.firstname),
+            sessionStorage.setItem("lastname", user.lastname),
+            sessionStorage.setItem("password", user.password);
+        }
+    } catch (error) {
+        console.log("error during getting user info:", error);
         throw error;
     }
 }
--- src/registreren.ts
+++ src/registreren.ts
@@ -1,12 +1,14 @@
 import "./config";
-import { api, session } from "@hboictcloud/api";
-import { createAccount, validateLogin } from "./databaseQuery";
+import { session } from "@hboictcloud/api";
+import { createAccount, validateLogin, getInfoData, checkEmailExists } from "./databaseQuery";
 
 
 
 
 const btn: any = document.getElementById("btnRegister") as HTMLButtonElement;
-btn.addEventListener("click", setup);
+if(btn) {
+    btn.addEventListener("click", setup);
+}
 
 async function setup(): Promise<void> {
 
@@ -16,49 +18,53 @@ async function setup(): Promise<void> {
     const lastname: string = (<HTMLInputElement>document.getElementById("lastname")).value;
     const password: string = (<HTMLInputElement>document.getElementById("password")).value;
 
-    const done: number | undefined = await createAccount(email, username, firstname, lastname, password);
-
-
-    if (done !== undefined) {
-        alert("Succesfull, registration complete!");
-        window.location.replace("./login.html");
+    const check: boolean | undefined = await checkEmailExists(email);
+    console.log(check);
+    if(check === false) {
+        const done: number | undefined = await createAccount(email, username, firstname, lastname, password);
+        if (done !== undefined) {
+            alert("Succesfull, registration complete!");
+            window.location.replace("./login.html");
+        }else {
+            console.log("Registration failed");
+            alert("Registration failed");
+        }
     }else {
-        console.log("Registration failed");
-        alert("Registration failed");
-    }
-    
+        console.log("registration failed, email already exists");
+        alert("registration failed, email already exists");
+    } 
 }
 
 const btnLogin: any = document.getElementById("btnLogin") as HTMLButtonElement;
-btnLogin.addEventListener("click", login);
+if(btnLogin) {
+    btnLogin.addEventListener("click", login);
+}
 
-async function login(): Promise<void> {
+async function login(): Promise<any> {
     const email: string = (<HTMLInputElement>document.getElementById("emailLogin")).value;
-    // const password: string = (<HTMLInputElement>document.getElementById("passwordLogin")).value;
+    const password: string = (<HTMLInputElement>document.getElementById("passwordLogin")).value;
 
-    const done: number | undefined = await validateLogin(email);
+    const done: number | undefined = await validateLogin(email, password);
 
+    if(done) {
+        const doneNew: string = String(done);
+        sessionStorage.setItem("userid", doneNew);
+        const getInfo: number = await getInfoData(done);
+
+        if(getInfo) {
+            window.location.replace("./main.html");
+        }
+    }
 }
 
 const btnLogout: any = document.getElementById("logout") as HTMLButtonElement;
-btnLogout.addEventListener("click", logout);
+if(btnLogout) {
+    btnLogout.addEventListener("click", logout);
 
+}
 function logout(): void {
-    const sessie: any = session.clear();
+    const sessie: any = sessionStorage.clear();
     if(sessie) {
         window.location.replace("/index.html");
     }
 }
-
-
-
-// session.set(email, "email");
-// session.set(username, "username");
-// session.set(firstname, "firstname");
-// session.set(lastname, "lastname");
-// session.set(password, "password");
-
-
-
-// const btn: any = document.getElementById("btnRegister") as HTMLButtonElement;
-// btn.addEventListener("click", register);
