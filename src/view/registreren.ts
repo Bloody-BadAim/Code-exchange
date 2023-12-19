e44b886837d10693a6593c59cecdcd62ee26ad08
Yusuf Ozturk <yusuf.ozturk3@hva.nl>
2023-12-19 19:53:45 +0100

    edit profile fixed

--- src/view/registreren.ts
+++ src/view/registreren.ts
@@ -1,8 +1,11 @@
 import "../config";
 import { UserController } from "../controller/userController";
 
+
 const userController : UserController = new UserController();
 
+// Gets all the elements needed
+// for the class usercontroller.register fucntion to work.
 document.getElementById("btnRegister")?.addEventListener("click", async () => {
     const email: string = (document.getElementById("email") as HTMLInputElement).value;
     const username: string = (document.getElementById("username") as HTMLInputElement).value;
@@ -16,6 +19,8 @@ document.getElementById("btnRegister")?.addEventListener("click", async () => {
         
     }
 });
+// gets all input values in login form.
+// then checks with usercontroller.login if it exists or not.
 document.getElementById("myFormLogin")?.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -30,6 +35,7 @@ document.getElementById("myFormLogin")?.addEventListener("submit", async (event)
     
     }
 });
+// makes you logout if button logout clicked
 document.getElementById("logout")?.addEventListener("click", () => {
     userController.logout();
 });
